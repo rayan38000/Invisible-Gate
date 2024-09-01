4ae79d5952c1c693f0f0a9760a0bcafbeb0569f3
Rayan <rayan.bkrs@proton.me>
2024-09-01 20:01:31 +0200

    Update config.php

--- config.php
+++ config.php
@@ -14,7 +14,7 @@ $redirection = 'https://google.com';
 // 🕵️ Setting up User Behavior Monitoring
 $userBehaviorMonitoring = True; // Enables/disables user behavior monitoring
 $suspiciousActionNumber = 2; // Number of suspicious action laws the user can violate before receiving an ip ban
-$timingBetwenRequests = 1; // Cooldown tolerated between 2 requests in seconds
+$timingBetwenRequests = 0.25; // Cooldown tolerated between 2 requests in seconds
 $useragentSwitcherDetector = True; // Detects user agent rotation
 $ipSwitcherDetector = True; // Detects proxy rotation
 $exceedRequests = 100 // Defines a 5-minute query limit
